2cb390a732ac589c66b19876eb6b4b607200fca8
Flesier <flesieronyango18@gmail.com>
2023-07-13 12:43:00 +0000

    add a country column

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- script that creates a table of users
+-- with id, email, name, country
+CREATE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
