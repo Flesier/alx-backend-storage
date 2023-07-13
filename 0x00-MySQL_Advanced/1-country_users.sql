1c3d013d7ad6d758d1f4cb186715b4c1de0dbbda
Flesier <flesieronyango18@gmail.com>
2023-07-13 16:14:25 +0000

    updated primary key

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- script that creates a table of users
 -- with id, email, name, country
 CREATE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-	PRIMARY KEY (id)
+	country ENUM('US', 'CO', 'TN')  DEFAULT 'US' NOT NULL,
 );
